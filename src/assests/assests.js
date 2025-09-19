016c62b20f1b71b297f7591421ea4016a10310e7
M.Umer Shafqat <umershafqat251786@gmail.com>
2025-09-19 15:31:14 +0500

    add bodypic

--- src/assests/assests.js
+++ src/assests/assests.js
@@ -2,10 +2,12 @@ import logo_1 from './logo_1.png';
 import location_logo from './location_logo.png'
 import search_icon from './search_icon.png'
 import cart_icon from './cart_icon.png'
+import mainpic from './mainpic.png'
 
 export const assests = {
   logo_1,
   location_logo,
   search_icon,
   cart_icon,
+  mainpic,
 };
--- src/components/Navbar1/Navbar1.jsx
+++ src/components/Navbar1/Navbar1.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { assests } from "../../assests/assests";
 import './Navbar1.css';
 
 function Navbar1() {
   return (
   <>
   <div className="navbar1-container">
-    
+     <div class="paraforpanel">
+       <p>Welcome to our store! 🎉 Big sale today — Up to 50% OFF on selected items! 🚀</p>
+    </div>
+    <div className="mainbody-pic">
+        <img src={assests.mainpic} alt="Main pic"/>
+    </div>
   </div>
   </>
   )
 }
 
-export default Navbar1
+export default Navbar1;
