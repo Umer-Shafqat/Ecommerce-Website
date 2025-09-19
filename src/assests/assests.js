d45cea7aa9b1f5bca7946ad5f12c0910ee735be3
M.Umer Shafqat <umershafqat251786@gmail.com>
2025-09-19 10:57:45 +0500

    add location icon

--- src/assests/assests.js
+++ src/assests/assests.js
@@ -1,5 +1,7 @@
 import logo_1 from './logo_1.png';
+import location_logo from './location_logo.png'
 
 export const assests = {
   logo_1,
+  location_logo,
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,15 @@ function Navbar() {
       <div className="navbar-img">
          <img src={assests.logo_1} alt="Navbar logo"/>
       </div>
-    
+    <div className="delivery">
+      <div className="para-1">
+      <p className='para-1'>Deliver to </p>
+      </div>
+      <div className="location">
+      <img className='location-img' src={assests.location_logo} alt="location" />
+      <p className='para-2'>Pakistan</p>
+      </div>
+    </div>
          </div>
     </>
   );
