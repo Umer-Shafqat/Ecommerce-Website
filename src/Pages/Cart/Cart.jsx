9b4d0806678f7a2b41ee7b7c2d02ba9b49735b54
M.Umer Shafqat <umershafqat251786@gmail.com>
2025-10-10 23:56:22 +0500

    edit files

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,13 +1,10 @@
 import { useContext } from "react";
 import "./Cart.css";
-import { useNavigate } from "react-router-dom";
 import { StoreContext } from "../../Context/StoreContext";
 
 const Cart = () => {
-  const { cartitems, display_items, removeFromCart, getTotalCartAmount } =
+  const { cartitems, display_items, removeFromCart } =
     useContext(StoreContext);
-
-  const navigate = useNavigate();
 
   return (
     <div className="cart">
@@ -45,46 +42,6 @@ const Cart = () => {
           }
           return null;
         })}
-      </div>
-
-      <div className="cart-bottom">
-        <div className="cart-total">
-          <h2>Cart Totals</h2>
-          <div>
-            <div className="cart-total-details">
-              <p>Sub Total</p>
-              <p>${getTotalCartAmount().toFixed(2)}</p>
-            </div>
-            <hr className="hr" />
-            <div className="cart-total-details">
-              <p>Delivery Fee</p>
-              <p>${getTotalCartAmount() === 0 ? 0 : 6}</p>
-            </div>
-            <hr className="hr" />
-            <div className="cart-total-details">
-              <b>Total</b>
-              <b>
-                $
-                {getTotalCartAmount() === 0
-                  ? 0
-                  : (getTotalCartAmount() + 6).toFixed(2)}
-              </b>
-            </div>
-          </div>
-          <button onClick={() => navigate("/order")}>
-            Proceed to Checkout
-          </button>
-        </div>
-
-        <div className="cart-promocode">
-          <div>
-            <p>If you have a Promo Code, enter it here</p>
-            <div className="cart-promocode-input">
-              <input type="text" placeholder="Enter your code" />
-              <button>Apply</button>
-            </div>
-          </div>
-        </div>
       </div>
     </div>
   );
